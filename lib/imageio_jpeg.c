7e2f70b86d1737c9506e51ffb698129f224a3c93
Andreas Eichner <aeichner@freenet.de>
2012-10-25 14:18:42 +0200

    change JPEG file extension for backward compatibility
    This commit sets the file extension for JPEG tiles to .jpeg to
    reuse caches originally seeded by TileCache.py.

--- lib/imageio_jpeg.c
+++ lib/imageio_jpeg.c
@@ -321,7 +321,7 @@ mapcache_image_format* mapcache_imageio_create_jpeg_format(apr_pool_t *pool, cha
 {
   mapcache_image_format_jpeg *format = apr_pcalloc(pool, sizeof(mapcache_image_format_jpeg));
   format->format.name = name;
-  format->format.extension = apr_pstrdup(pool,"jpg");
+  format->format.extension = apr_pstrdup(pool,"jpeg");
   format->format.mime_type = apr_pstrdup(pool,"image/jpeg");
   format->format.metadata = apr_table_make(pool,3);
   format->format.create_empty_image = _mapcache_imageio_jpg_create_empty;
